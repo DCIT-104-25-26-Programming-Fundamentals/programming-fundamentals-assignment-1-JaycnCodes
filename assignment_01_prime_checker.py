3095fa87aef913e11ef2e428123c096c282e8b1b
JaycnCodes <jaafari004@st.ug.edu.gh>
2026-07-26 20:35:42 +0000

    Complete prime checker implementation

--- assignment_01_prime_checker.py
+++ assignment_01_prime_checker.py
@@ -34,4 +34,24 @@
 # =============================================================================
 # YOUR CODE BELOW — remove the # symbols from the scaffold and fill it in
 # =============================================================================
+def is_prime(number):
+    if number<2:
+        return False
+
+    for i in range(2,int(number**0.5)+1):
+        if number%i==0:
+            return False
+    return True
+
+if __name__=="__main__":
+        try:
+            user_input= int (input("Enter a number: "))
+
+            if is_prime(user_input):
+                print(f"{user_input}is a prime number")
+            else:
+                print(f"{user_input} is not a prime number.")
+
+        except ValueError:
+            print("Please enter a valid whole number.")        
 
